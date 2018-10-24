1b122664fbb502ff2bfff9911547fbee32328247
sandemid <41757583+sandemid@users.noreply.github.com>
2018-10-24 17:33:07 +0300

    Реализована структура данных Простого связанного списка с итератором, класс тестирующий его функционал

--- src/sandem/java2/lesson4/LinkedListInterface.java
+++ src/sandem/java2/lesson4/LinkedListInterface.java
@@ -1,4 +1,23 @@
 package sandem.java2.lesson4;
 
-public interface LinkedListInterface {
+public interface LinkedListInterface extends Iterable<Integer>{
+
+    void add(int value);
+
+    Link remove();
+
+    boolean isEmpty();
+
+    int getSize();
+
+    Link search(int value);
+
+    Link remove(int value);
+
+    void display();
+
+    Link getFirstElement();
+
+    void setFirstElement(Link firstElement);
+
 }
--- src/sandem/java2/lesson4/LinkedListIterator.java
+++ src/sandem/java2/lesson4/LinkedListIterator.java
@@ -2,15 +2,86 @@ package sandem.java2.lesson4;
 
 import java.util.Iterator;
 
-public class LinkedList implements Iterator<Integer> {
+public class LinkedListIterator implements Iterator<Integer> {
+
+    private LinkedListInterface list;
+    private boolean firstIterate = true;
+
+    private Link current;
+    private Link previous;
+
+    public LinkedListIterator(LinkedListInterface list) {
+        this.list = list;
+    }
 
     @Override
     public boolean hasNext() {
-        return false;
+        if (firstIterate) {
+            firstIterate = false;
+            return list.getFirstElement() != null;
+        }
+        else {
+            return current.getNext() != null;
+        }
     }
 
     @Override
     public Integer next() {
-        return null;
+        if (current == null) {
+            current = list.getFirstElement();
+        }
+        else {
+            previous = current;
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+
+    @Override
+    public void remove() {
+        if (previous == null){
+            list.setFirstElement(current.getNext());
+            reset();
+        } else {
+            previous.setNext(current.getNext());
+            if ( !hasNext() ) {
+                reset();
+            } else {
+                current = current.getNext();
+            }
+        }
+    }
+
+    public void reset() {
+        firstIterate = false;
+        current = list.getFirstElement();
+        previous = null;
+    }
+
+    public void insertBefore(int value) {
+        Link newItem = new Link(value);
+        if(previous == null) {
+            newItem.setNext(list.getFirstElement());
+            list.setFirstElement(newItem);
+            reset();
+        }
+        else {
+            newItem.setNext(previous.getNext());
+            previous.setNext(newItem);
+            current = newItem;
+        }
+
+    }
+
+    public void insertAfter(int value) {
+        Link newItem = new Link(value);
+        if (list.isEmpty()){
+            list.setFirstElement(newItem);
+            current = newItem;
+        } else {
+            newItem.setNext(current.getNext());
+            current.setNext(newItem);
+            next();
+        }
     }
 }
