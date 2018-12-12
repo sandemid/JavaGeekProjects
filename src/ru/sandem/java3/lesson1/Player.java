8433c3e24be5fddb07a7445bb0c8f385ccae0a50
Alexander Demidenkov <san_dem@inbox.ru>
2018-12-12 21:52:48 +0300

    Реализовано: 1. Класс Игрока 2. Каркас класса команды с конструктором

--- src/ru/sandem/java3/lesson1/Player.java
+++ src/ru/sandem/java3/lesson1/Player.java
@@ -1,4 +1,28 @@
 package ru.sandem.java3.lesson1;
 
 public class Player {
+
+    private String name;
+    private int canOvercomeBarrier;
+
+    public Player(String name, int canOvercomeBarrier) {
+        this.name = name;
+        this.canOvercomeBarrier = canOvercomeBarrier;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCanOvercomeBarrier(int canOvercomeBarrier) {
+        this.canOvercomeBarrier = canOvercomeBarrier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCanOvercomeBarrier() {
+        return canOvercomeBarrier;
+    }
 }
--- src/ru/sandem/java3/lesson1/Team.java
+++ src/ru/sandem/java3/lesson1/Team.java
@@ -1,4 +1,24 @@
 package ru.sandem.java3.lesson1;
 
 public class Team {
+
+    private final int TEAM_SIZE = 4;
+    private Player[] players = new Player[TEAM_SIZE];
+    private String teamName;
+    private int[] passedDistance = new int[TEAM_SIZE];
+
+    public Team(String teamName, Player ... players) {
+        for (int i = 0; i < TEAM_SIZE; i++) {
+            this.players[i] = players[i];
+        }
+        this.teamName = teamName;
+    }
+
+    public void showTeam (){
+
+    }
+
+    public void showResults(){
+
+    }
 }
